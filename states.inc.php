06399319a75422977f1e25c79ee358e68742c9c4
harpwn <hbill1337@gmail.com>
2026-04-24 11:54:21 +0100

    remove unused file

--- states.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Bga\GameFramework\GameStateBuilder;
-use Bga\GameFramework\StateType;
-
-// States are now defined as PHP classes in modules/php/States/.
-// This file is kept for compatibility but is no longer the source of truth.
-$machinestates = [];
